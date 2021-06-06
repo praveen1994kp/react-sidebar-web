75c69ff6f83df000bfe9fc88b39ef642b2560c88
Praveen Kumaravel <praveen1994kp@gmail.com>
2021-06-06 23:00:32 +0530

    fixed import

--- example/src/Application.js
+++ example/src/Application.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import styled from 'styled-components'
-import {Header, Sidebar, NavItemsContainer, NavItem} from 'react-sidebar'
+import {Header, Sidebar, NavItemsContainer, NavItem} from 'react-sidebar-web'
 import { Pages } from './Pages'
 import BrandIcon from './samples/BrandIcon'
 
